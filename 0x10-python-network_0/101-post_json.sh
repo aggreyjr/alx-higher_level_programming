a9d3335d7afd253f5180749490b31665db955e38
aggreyjr <tukashabaaggrey89@gmail.com>
2023-09-30 01:32:14 +0300

    n

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed as the first argument, and displays the body
-
-curl -s "$1" -X POST -H "Content-Type: application/json" -d @"$2" 
+# Sends a JSON POST request to a given URL with a given JSON file.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
